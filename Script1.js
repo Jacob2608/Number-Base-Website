7237355c65f80deb129702c29a1931ba84750130
Jacob2608 <jacob.davis2608@gmail.com>
2024-03-10 12:17:37 +0000

    Added binary converter and information

--- Script1.js
+++ Script1.js
@@ -1,1 +1,28 @@
 // JavaScript source code
+function binary(){
+    let numberD = document.getElementById('binaryNumber').value; //get input
+
+    //calculates required digits
+    let digits = 1;
+    while (2 ** digits - 1 < Number(numberD)){
+        digits++;
+    }
+
+    //assembles binary number
+    let numberB = '';
+    let numberCheck = 0;
+    for (let i = digits; i >= 1; i--){
+        if (numberCheck + 2 ** (i - 1) > numberD){
+            numberB += '0';
+        }
+        else{
+            numberB += '1';
+            numberCheck += 2 ** (i - 1);
+        }
+    }
+
+    //output
+    document.getElementById('binaryOutput').innerHTML = String(numberB);
+}
+
+binaryBtn.addEventListener('click', binary);
